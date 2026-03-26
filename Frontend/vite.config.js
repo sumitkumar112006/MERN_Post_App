fa3ae74581b8ee7ae8ed58e6b661105ca9d0b6d2
sumitkumar112006 <sumitkumar112006@users.noreply.github.com>
2026-03-26 17:40:45 +0530

    Fix Vite base path for Vercel

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/MERN_Post_App/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
